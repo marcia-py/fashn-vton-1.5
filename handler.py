b6828f90838458bc7243d7c481b386c0f3954ab3
Márcia Castro <marciafcastro02@gmail.com>
2026-07-16 19:37:40 +0100

    Add logging and initialize TryOnPipeline
    Added print statements to indicate processing steps and initialized TryOnPipeline.

--- handler.py
+++ handler.py
@@ -1,14 +1,17 @@
-import requests
 import runpod
-from PIL import Image
-from utils import upload_to_r2
 from fashn_vton import TryOnPipeline
 
 print("STEP 1")
 
+print("STEP 2")
+pipeline = TryOnPipeline(
+    weights_dir="./weights"
+)
+
+print("STEP 3")
+
 
 def handler(job):
     return {"success": True}
-
 
 runpod.serverless.start({"handler": handler})
